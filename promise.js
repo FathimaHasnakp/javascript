4aed1477e1d0045e5155dadde82258ec9608bc00
FathimaHasnakp <fathimahasna738@gmail.com>
2025-09-08 09:15:18 +0530

    committed

--- promise.js
+++ promise.js
@@ -1,3 +1,10 @@
 // const promise = new promise((resolve, reject)) => {
 //     //asynchronous operation 
 // }
+
+//resolve called when the function is successful//
+//reject called when the function is failure
+
+//.then = execute the promise is resolve
+//.catch = execute the promis is rejected
+//.fainaly = execute if the promis is resolved or rejected
