640bba81e27801a851b879dee83a8ea7a9f80cab
FathimaHasnakp <fathimahasna738@gmail.com>
2025-09-10 10:55:39 +0530

    commited

---
+++ Spread-operators/arrays/adding-items.js
@@ -0,0 +1,3 @@
+const fruits = ["banana","mango"];
+const moreFruits =["apple" ,...fruits,"ornage"];
+console.log(moreFruits);
---
+++ rest-parameter/rest-with-named-parametr.js
@@ -0,0 +1,7 @@
+function greet (firstname, lastname,...titles) {
+    console.log(`name: ${firstname} ${lastname}`);
+    console.log(`titles: ${titles.join(',')}`);
+    
+    
+}
+greet("fathima", "hasna", "BCA", "software engineer");
